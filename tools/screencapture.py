e406f953764183de6f7d8fffe83be1ccda6d512f
Shawn Lee <dazhu60@163.com>
2014-08-25 23:15:49 +0800

    update the screen capture

--- tools/screencapture.py
+++ tools/screencapture.py
@@ -5,8 +5,11 @@ import subprocess
 
 
 class ScreenCap:
+    ## Local path to save the picture
     SAVEPATH = r"~/"
+    ## Picture file name base
     FILE_NAME = "screencap"
+    ## Device path to save the picture temporally
     DEVICE_PATH = r"/sdcard/"
 
     def __init__(self, filename = None, savepath = None):
@@ -16,18 +19,18 @@ class ScreenCap:
             self.SAVEPATH = savepath
 
     def run(self):
-        cur_time = time.strftime("%Y%m%d-%H%M%S");
+        cur_time = time.strftime("%Y%m%d-%H%M%S")
         an_path = self.DEVICE_PATH + self.FILE_NAME + cur_time + ".png"
         an_cmd = r"adb shell screencap -p " + an_path
         pull_cmd = r"adb pull " + an_path + " " + self.SAVEPATH
         del_cmd = r"adb shell rm " + an_path
-        subprocess.Popen(an_cmd,shell=True).communicate();
+        subprocess.Popen(an_cmd,shell=True).communicate()
 
         if not os.path.exists(self.SAVEPATH):
             os.makedirs(self.SAVEPATH)
 
-        subprocess.Popen(pull_cmd,shell=True).communicate();
-        subprocess.Popen(del_cmd,shell=True).communicate();
+        subprocess.Popen(pull_cmd,shell=True).communicate()
+        subprocess.Popen(del_cmd,shell=True).communicate()
 
 
 if __name__ == '__main__':
